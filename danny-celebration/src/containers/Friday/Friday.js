fee8792fa6b2043e79ffbf8a558a6931203fbfb8
Jennifer Schmiett <bailliejm@gmail.com>
2022-09-23 14:22:55 -0500

    Adding text info to Pasta Dinner

--- danny-celebration/src/containers/Friday/Friday.js
+++ danny-celebration/src/containers/Friday/Friday.js
@@ -7,9 +7,9 @@ export default function Footer() {
                 <h2>Pasta Dinner</h2>
             </div>
             <div>
-                <p>Pasta Dinner</p>
+                <p>Hosted by Lee and Vicki Hall</p>
+                <p>Serving Pasta, Salad, and Garlic Bread</p>
             </div>
         </>
-
     )
 }
