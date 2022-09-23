2cde12c4e6568f5774f71738ccf47eec287d5787
Jennifer Schmiett <bailliejm@gmail.com>
2022-09-23 14:20:27 -0500

    Adding title to Header.js

--- danny-celebration/src/components/Header/Header.js
+++ danny-celebration/src/components/Header/Header.js
@@ -4,8 +4,13 @@ import NavTabs from './NavTabs';
 export default function Header({ currentPage, setCurrentPage }) {
     const handlePageChange = (page) => setCurrentPage(page);
     return (
-        <div>
-            <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-        </div>
+        <>
+            <div>
+                <h2>Danny's Celebration of Life</h2>
+            </div>
+            <div>
+                <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+            </div>
+        </>
     )
 }
