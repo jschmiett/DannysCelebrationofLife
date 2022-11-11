09b080a054689feda437a797e2d3a8f90ed82793
Jennifer Schmiett <bailliejm@gmail.com>
2022-11-11 12:28:20 -0600

    Added top margin to placeholder img

--- danny-celebration/src/containers/Danny/Danny.js
+++ danny-celebration/src/containers/Danny/Danny.js
@@ -4,7 +4,7 @@ export default function Danny() {
     return (
         <>
             <div>
-                <img src="https://image-placeholder.com/images/actual-size/320x320.png"></img>
+                <img className="danny-image" src="https://image-placeholder.com/images/actual-size/320x320.png"></img>
             </div>
             <div>
                 <h2>About Danny</h2>
